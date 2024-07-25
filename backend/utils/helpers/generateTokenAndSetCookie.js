fe059c6ed11300ef6f4e529faa868f1b96a726da
praneethmareddy <112878756+praneethmareddy@users.noreply.github.com>
2024-07-26 02:05:25 +0530

    Update generateTokenAndSetCookie.js

--- backend/utils/helpers/generateTokenAndSetCookie.js
+++ backend/utils/helpers/generateTokenAndSetCookie.js
@@ -2,12 +2,12 @@ import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
+		
 	});
 
 	res.cookie("jwt", token, {
 		httpOnly: true, // more secure
-		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+		
 		sameSite: "strict", // CSRF
 	});
 
