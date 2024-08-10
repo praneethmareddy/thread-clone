7a87226a8c721fb2b0fc514a277777f15c22451b
praneethmareddy <112878756+praneethmareddy@users.noreply.github.com>
2024-08-11 01:05:41 +0530

    Update cron.js

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://thread-clone-gb3b.onrender.com/";
+const URL = "https://thread-clone-gb3b.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
