99eb5ec28dc4bd2f7cdb93f595ac21bfe8916473
praneethmareddy <112878756+praneethmareddy@users.noreply.github.com>
2024-10-02 18:31:02 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,6 +33,10 @@ const getUserProfile = async (req, res) => {
 const signupUser = async (req, res) => {
 	try {
 		const { name, email, username, password } = req.body;
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (!emailRegex.test(email)) {
+			return res.status(400).json({ error: "Invalid email format" });
+		}
 		const user = await User.findOne({ $or: [{ email }, { username }] });
 
 		if (user) {
